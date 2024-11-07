c6e14ae63be566c17227acb62915d6a6e00bc141
saifuddinmonna <saifuddinmonna@gmail.com>
2024-11-07 20:56:47 +0600

    creatring enroolment table and inserting sampledata

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -18,3 +18,23 @@ CREATE TABLE courses (
     credits INT CHECK (credits > 0)
 );
 
+-- Create enrollment table
+CREATE TABLE enrollment (
+    enrollment_id INT PRIMARY KEY,
+    student_id INT,
+    course_id INT,
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
+
+
+-- Insert sample data into students
+
+INSERT INTO students (student_id, student_name, age, email, frontend_mark, backend_mark, status)
+VALUES
+(1, 'Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+(2, 'Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+(3, 'Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+(4, 'Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+(5, 'Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+(6, 'Hasan', 23, 'hasan@gmail.com', NULL, NULL, NULL);
